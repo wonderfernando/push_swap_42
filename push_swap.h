82d31c2e5f2b037f2e87f2df40164eb699aa424b
Fernando Da Silva <ferda-si@c1r3s1.42luanda.com>
2024-10-21 20:05:51 +0100

    norminette header

--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ferda-si <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 19:53:30 by ferda-si          #+#    #+#             */
+/*   Updated: 2024/10/21 19:53:34 by ferda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -7,50 +19,49 @@
 
 typedef struct s_stack
 {
-	long            nbr;
-	long            index;
-	long            key_nbr;
-	long            tm_aux;
-	struct s_stack  *next;
-} t_stack;
+	long			nbr;
+	long			index;
+	long			key_nbr;
+	long			tm_aux;
+	struct s_stack	*next;
+}	t_stack;
 
-t_stack *ft_stack_lstlast(t_stack *lst);
-void	ft_error(void);
-void	ft_ra(t_stack **a, int j);
-void	ft_sa(t_stack **a, int j);
-void	ft_pa(t_stack **a, t_stack **b, int j);
-void	ft_rra(t_stack **a, int j);
-void	ft_ss(t_stack **a, t_stack **b, int j);
-int			ft_atoi_limit(const char *str);
-t_stack		*ft_init_sub(char **argv);
-void	push(t_stack **stack, t_stack *stack_new);
-t_stack		*ft_stack_new(int content);
-t_stack		*ft_init(int argc, char **argv);
-int			ft_isdigit(int c);
-size_t		ft_strlen(const char *s);
-char		**ft_split(char const *s, char c);
-static	int		count_words(const char *s, char c);
-static	char    *word_dup(const char *s, int start, int end);
-void		ft_freestr(char **str);
-void		ft_free(t_stack **lst);
-int			ft_checkdup(t_stack *a);
-int			ft_checksorted(t_stack *stack_a);
-void	fill_index(t_stack **stack);
-int			ft_stack_lstsize(t_stack *lst);
-int			ft_min(t_stack *a);
-void	ft_pb(t_stack **stack_a, t_stack **stack_b);
-int			ft_max(t_stack *a);
-int			ft_find_index(t_stack *a, int nbr);
-void	aproximacao(t_stack **a);
-void	aproximacao2(t_stack **b);
-void	ft_rrb(t_stack **b, int j);
-void	ft_rb(t_stack **b, int j);
-void	sort_true(t_stack **a);
-void	order_ten(t_stack **pilha_a, t_stack **pilha_b);
-long	particiona(long *v, long inicio, long fim);
-void	quicksort(long *v, long inicio, long fim);
-long	*get_vetor_sort(t_stack *a, long *tm);
-void	sort_aux(t_stack **pilha_a, t_stack **pilha_b);
-void	order_hundred(t_stack **pilha_a, t_stack **pilha_b, long diviser);
+long			particiona(long *v, long inicio, long fim);
+long			*get_vetor_sort(t_stack *a, long *tm);
+int				ft_atoi_limit(const char *str);
+int				ft_isdigit(int c);
+int				ft_checkdup(t_stack *a);
+int				ft_checksorted(t_stack *stack_a);
+int				ft_stack_lstsize(t_stack *lst);
+int				ft_min(t_stack *a);
+int				ft_max(t_stack *a);
+int				ft_find_index(t_stack *a, int nbr);
+size_t			ft_strlen(const char *s);
+void			ft_error(void);
+void			ft_ra(t_stack **a, int j);
+void			ft_sa(t_stack **a, int j);
+void			ft_pa(t_stack **a, t_stack **b, int j);
+void			ft_rra(t_stack **a, int j);
+void			ft_ss(t_stack **a, t_stack **b, int j);
+void			push(t_stack **stack, t_stack *stack_new);
+char			**ft_split(char const *s, char c);
+void			ft_freestr(char **str);
+void			ft_free(t_stack **lst);
+void			fill_index(t_stack **stack);
+void			ft_pb(t_stack **stack_a, t_stack **stack_b);
+void			aproximacao(t_stack **a);
+void			aproximacao2(t_stack **b);
+void			ft_rrb(t_stack **b, int j);
+void			ft_rb(t_stack **b, int j);
+void			sort_true(t_stack **a);
+void			order_ten(t_stack **pilha_a, t_stack **pilha_b);
+void			quicksort(long *v, long inicio, long fim);
+void			sort_aux(t_stack **pilha_a, t_stack **pilha_b);
+void			order_hundred(t_stack **pilha_a, \
+				t_stack **pilha_b, long diviser);
+t_stack			*ft_stack_lstlast(t_stack *lst);
+t_stack			*ft_init_sub(char **argv);
+t_stack			*ft_stack_new(int content);
+t_stack			*ft_init(int argc, char **argv);
 
 #endif
--- utils_two.c
+++ utils_two.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_two.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ferda-si <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 19:51:46 by ferda-si          #+#    #+#             */
+/*   Updated: 2024/10/21 19:51:49 by ferda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "push_swap.h"
 
 t_stack	*ft_stack_lstlast(t_stack *lst)
@@ -8,6 +19,7 @@ t_stack	*ft_stack_lstlast(t_stack *lst)
 		lst = lst->next;
 	return (lst);
 }
+
 void	ft_error(void)
 {	
 	write (2, "Error\n", 6);
@@ -27,7 +39,6 @@ int	ft_stack_lstsize(t_stack *lst)
 	return (i);
 }
 
-
 int	ft_min(t_stack *a)
 {
 	int	i;
@@ -41,6 +52,7 @@ int	ft_min(t_stack *a)
 	}
 	return (i);
 }
+
 int	ft_max(t_stack *a)
 {
 	int	i;
